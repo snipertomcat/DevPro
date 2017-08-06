0f40f782137cdeab29085a41aed70a42ce35f377
Jesse Griffin <hemham914@gmail.com>
2017-08-06 03:18:18 -0700

    Initial Commit

---
+++ resources/lang/fr/history.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | History Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain strings associated to the
+    | system adding lines to the history table.
+    |
+    */
+
+    'backend' => [
+        'none'            => 'Aucun historique récent.',
+        'none_for_type'   => 'Aucun historique pour ce type.',
+        'none_for_entity' => 'Aucun historique pour :entity.',
+        'recent_history'  => 'Historique récent',
+
+        'roles' => [
+            'created' => 'a créé le rôle',
+            'deleted' => 'a effacé le rôle',
+            'updated' => 'a mis à jour le rôle',
+        ],
+        'users' => [
+            'changed_password'    => 'a modifié le mot de passe de l\'utilisateur',
+            'confirmed' => 'confirmed user',
+            'created'             => 'a créé l\'utilisateur',
+            'deactivated'         => 'a désactivé l\'utilisateur',
+            'deleted'             => 'a effacé l\'utilisateur',
+            'deleted_social'      => 'deleted social account',
+            'permanently_deleted' => 'a définitivement effacé l\'utilisateur',
+            'updated'             => 'a mis à jour l\'utilisateur',
+            'unconfirmed' => 'un-confirmed user',
+            'reactivated'         => 'a réactivé l\'utilisateur',
+            'restored'            => 'a restauré l\'utilisateur',
+        ],
+    ],
+];
